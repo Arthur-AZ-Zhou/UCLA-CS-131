820eb211c2856b462485cad4feeb400e6513f188
Arthur Zhou <arthuracct@gmail.com>
2024-10-16 20:08:02 -0700

    saving work for now

--- Project 1/fall-24-project-starter-main/fall-24-project-starter-main/interpreterv1.py
+++ Project 1/fall-24-project-starter-main/fall-24-project-starter-main/interpreterv1.py
@@ -6,65 +6,92 @@ from element import Element
 class Interpreter(InterpreterBase):
     def __init__(self, console_output = True, inp = None, trace_output = False):
         super().__init__(console_output, inp)
-        self.trace_output = trace_output
-        self.variables = {} #store names and values
+        self.trace_output = trace_output # it's whatever if I have this
+        self.name_to_item = dict()
 
     def run(self, program):
         ast = parse_program(program)
         print("ast: ", ast)
-        self.variable_val_dict = dict()
-        main_func_node = self.get_main_func_node(ast)
-        self.run_func(main_func_node)
-        # self.lines = program
-        # self.interpret_program()
+        main_func_node = self.get_main_func(ast)
+        self.run_func_def_node(main_func_node)
 
-    # def interpret_program(self): #looks for main function
-    #     main_func = None
-    #     for l in self.lines:
-    #         if l.startswith("func main"):
-    #             main_func = l
-    #             break
-
-    #     if main_func == None:
-    #         super().error(ErrorType.NAME_ERROR, "main() wasn't found")
-    #     else:
-    #         self.interpret_main(main_func)
-
-    # def interpret_main(self, body):
-    #     body = body.replace('func main() {', '').replace('}', '').strip()
-    #     lines = [l.strip() for l in body.split(';') if l.strip()]
-
-    #     for l in lines:
-    #         self.interpret_line(l)
-
-    # def interpret_line(self, line):
-        pass
-
-    def get_main_func_node(self, tree: Element):
-        if tree.elem_type == "program":
-            main = tree.get("functions")[0]
+    def get_main_func(self, tree): # SEARCHES THRU PROGRAM NODE
+        if tree.elem_type == "program": #We are guaranteed this too but this is just in case for future projects
+            main = tree.get("functions")[0] #this SHOULD ALWAYS WORK
 
             if main.elem_type == "func" and main.get("name") == "main":
                 print("SUCCESSFULLY FOUND MAIN")
                 return main
-            super().error(
-                ErrorType.NAME_ERROR,
-                "No main() function was found",
-            )
+            else:
+                super().error(ErrorType.NAME_ERROR, "No main() function was found",)
         return None
 
-    def run_func(self, main_node: Element):
+    def run_func_def_node(self, main_node): # RUN EACH STATEMENT IN FUNCTION DEFINITION NODE
         if main_node: #Not none
             for statement in main_node.get("statements"):
-                self.run_statement(statement)
+                self.run_each_statement(statement)
+            print(self.name_to_item)
 
-    def run_statement(self, node):
-        print("ElemType: ", node.elem_type)
+    def run_each_statement(self, node): # ANALYZE STATEMENT AND DO RESPECTIVE ACTION
+        print("\nElemType: ", node.elem_type)
+        print("Statement: ", node)
+        statement_type = node.elem_type
+        variable_name = node.get("name")
+
+        if statement_type == "vardef":
+            if variable_name in self.name_to_item:
+                super().error(ErrorType.NAME_ERROR, f"Variable {variable_name} defined more than once",)
+            self.name_to_item[variable_name] = None
+
+        elif statement_type == "=": 
+            if variable_name not in self.name_to_item:
+                super().error(ErrorType.NAME_ERROR, f"Variable {variable_name} has not been defined",)
+
+            expression_result = self.run_expression(node.get("expression"))
+            self.name_to_item[variable_name] = expression_result
+
+        elif statement_type == "fcall":
+            pass
+
+        else: #error if it's not a valid statement
+            super().error(ErrorType.NAME_ERROR, "Not a valid statement!",)
+
+    def run_expression(self, node): #PROCESS EACH EXPRESSION
+        expression_type = node.elem_type
+        print("EXPRESSION TYPE: ", expression_type, "---------------------------------------")
+
+        if expression_type == "+" or expression_type == "-":
+            op1 = self.run_expression(node.get("op1"))
+            op2 = self.run_expression(node.get("op2"))
+
+            if expression_type == "+":
+                return op1 + op2
+            elif expression_type == "-":
+                return op1 - op2
 
 test_program = """func main() {
     var x;
+    var y;
+    var z;
+    var a;
+    var b;
+    var a_str;
+    var magic_num;
+
     x = 5 + 6;
+    y = 10;
+    z = (x + (1 - 3)) - y;
+
+    a_str = "this is a string";
+    magic_num = inputi("enter a magic number: "); 
+
+    a = 4 + inputi("enter a number: ");
+    b = 3 - (3 + (2 + inputi()));
+
+    print(10);
+    print("hello world!");
     print("The sum is: ", x);
+    print("the answer is: ", x + (y - 5), "!");
 }"""
 
 new_interpreter = Interpreter(console_output = True, inp = None, trace_output = True)
